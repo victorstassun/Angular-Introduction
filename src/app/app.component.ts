7db786f10691450863a87c9938bd065c46e81849
Victor Stassun <victorstassun@gmail.com>
2022-03-31 11:02:54 -0300

    Criação do caminho de rotas, manipulação de dados, etc.

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent {
+
+  constructor() { }
+
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
